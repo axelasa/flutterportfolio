8ccce5be8093f54cc392165b3b23abd259017ae5
axelasa <asaaxel82@gmail.com>
2024-11-22 09:39:56 +0300

    updated tiktok link

--- lib/constants/sns_links.dart
+++ lib/constants/sns_links.dart
@@ -1,7 +1,7 @@
 class SnsLinks{
   static const String github = "https://github.com/axelasa";
   static const String linkedIn = "https://www.linkedin.com/in/axel-asa-855203141/";
-  static const String tiktok = "https://www.tiktok.com/@axelasa0";
+  static const String tiktok = "https://www.tiktok.com/@axelasa?lang=en";
   static const String instagram = "https://www.instagram.com/axelasa/";
   static const String X = "https://x.com/AxelAsa2";
   //static const String email = "https://www.shohruhak.com/tech";
